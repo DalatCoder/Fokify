47fcb5958ee6ae2f732ece54481b13c47c1bee7a
DalatCoder <hieuntctk42@gmail.com>
2019-06-01 22:58:40 +0700

    Building the Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,4 @@
+import Search from './models/Search';
 
-import axios from 'axios'
-
-async function getResult(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = 'ff3b8b3b389cebebb0acb5214f3144de';
-  const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-  const recipes = res.data.recipes;
-  console.log(recipes);
-}
-getResult('pho');
+const search = new Search('pizza');
+search.getResult();
